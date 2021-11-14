9b92f8458ccf4ec644d16c25a20990aa1563cb7d
Yasin Caliskan <yasin.clsk@hotmail.com>
2021-11-15 00:36:14 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            carManager.Add(new Car { Id = 5, BrandId = 2, ColorId = 1, ModelYear = 2021, DailyPrice = 4000, Description = "BMW" });
+            foreach (var item in carManager.GetList())
+            {
+                Console.WriteLine(item.Description);
+            }
+        }
+    }
+}
